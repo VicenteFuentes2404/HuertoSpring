74d61dc9792eb01ebb4270403d24473ddefc0395
Vicente Fuentes <fenix.fuentes2004@gmail.com>
2025-11-24 23:32:20 -0300

    Corrección menor

--- huerto/src/main/java/com/example/huerto/huerto/model/Producto.java
+++ huerto/src/main/java/com/example/huerto/huerto/model/Producto.java
@@ -21,10 +21,9 @@ public class Producto {
 
     @Lob 
     @Column(name = "imagen", columnDefinition = "BLOB")
-    private byte[] imagen; // Imagen principal (sin cambios)
+    private byte[] imagen; // Imagen principal
 
 
-    // 2. AÑADIR ESTE BLOQUE
     @ElementCollection // Le dice a JPA que es una colección
     @CollectionTable(
         name = "producto_imagenes", // Nombre de la nueva tabla para la galería
